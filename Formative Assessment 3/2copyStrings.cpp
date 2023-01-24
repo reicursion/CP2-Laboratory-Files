3476c2b931bb276366529defffcf79769e68ddbc
rei <202210643@fit.edu.ph>
2023-01-24 18:48:50 +0800

    copy str1 to str2 complete

---
+++ Formative Assessment 3/2copyStrings.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+void titleBar(); //function for the title bar
+
+int main(){
+    // initialize values
+    char str1[100];
+    char str2[100];
+    string str3;
+    int res;
+
+    titleBar();
+
+    // get user input for first word (str1)
+    cout << "\nEnter a first word (str1): ";
+    cin.getline(str1, 100);
+    // get user input for second word (str2)
+    cout << "Enter a second word (str2): ";
+    cin.getline(str2, 100);
+
+    // use compare() function to copy str2 to str1
+    strcpy(str1, str2);
+
+    // output the value of str1
+    cout << "new string value for str1: " << str1 << endl;
+
+    return 0;
+}
+
+
+// function for the title bar
+void titleBar(){
+    for (int i = 0; i < 30; i++){
+        cout << '*';
+    }
+
+    cout << "\nSTRING COPY\n";
+
+    for (int i = 0; i < 30; i++){
+        cout << '*';
+    }
+}
