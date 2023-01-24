772a5b9fc7f8f8339a0040ea704b60d314267a01
rei <202210643@fit.edu.ph>
2023-01-24 17:41:23 +0800

    completed the 1compareStrings

--- Formative Assessment 3/1compareStrings.cpp
+++ Formative Assessment 3/1compareStrings.cpp
@@ -2,11 +2,32 @@
 #include<string>
 using namespace std;
 
-void titleBar; //function for the title bar
+void titleBar(); //function for the title bar
 
 int main(){
+    // initialize values
+    string str1;
+    string str2;
+    int res;
+
     titleBar();
 
+    // get user input for first word (str1)
+    cout << "\nEnter a first word (str1): ";
+    cin >> str1;
+    // get user input for second word (str2)
+    cout << "Enter a second word (str2): ";
+    cin >> str2;
+
+    // use compare() function to compare 2 strings
+    res = str1.compare(str2);
+
+    // output if the two strings are equal (positive) or not (negative)
+    if (res == 0){
+        cout << "positive" << endl;
+    } else {
+        cout << "negative" << endl;
+    }
 
     return 0;
 }
