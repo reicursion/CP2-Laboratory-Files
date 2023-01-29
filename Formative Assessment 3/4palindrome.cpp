d717029b1a6d1bda0c6ba17efd2a64e49bec3c93
rei <202210643@fit.edu.ph>
2023-01-29 18:12:27 +0800

    for loop to reverse the inputted word

--- Formative Assessment 3/4palindrome.cpp
+++ Formative Assessment 3/4palindrome.cpp
@@ -1,10 +1,38 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 
+void titleBar(); //function for the title bar
+
 int main(){
+    // initialize values
+    char str1[100];
+    char str2[100];
 
+    titleBar();
 
+    // ask user input for a word
+    cout << "Enter a word: ";
+    cin.getline(str1,100);
 
+    // reverse the word and store it in a new variable
+    for (int i = strlen(str1) - 1; i >= 0; i--){
+        str2[strlen(str1) - 1 - i] = str1[i];
+    }
 
     return 0;
+}
+
+
+// function for the title bar
+void titleBar(){
+    for (int i = 0; i < 30; i++){
+        cout << '*';
+    }
+
+    cout << "\nPALINDROME\n";
+
+    for (int i = 0; i < 30; i++){
+        cout << '*';
+    }
 }
