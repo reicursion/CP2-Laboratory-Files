5441eda2c7802c5226521c39cf91e237c37e68c3
rei <202210643@fit.edu.ph>
2023-01-29 22:17:43 +0800

    started the first part

---
+++ Formative Assessment 3/6stringsToWords.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    // initialize variables
+    string word[100];
+
+    cout << "Enter a string: ";
+    getline(word, 100);
+
+
+}
