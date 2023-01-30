cb14e3478311e77b719721fd6d0d72b5b8911ce3
rei <202210643@fit.edu.ph>
2023-01-30 20:50:27 +0800

    code

--- Formative Assessment 3/6stringsToWords.cpp
+++ Formative Assessment 3/6stringsToWords.cpp
@@ -1,13 +1,28 @@
 #include<iostream>
-#include<cstring>
+#include<string>
 using namespace std;
 
 int main(){
     // initialize variables
-    string word[100];
+	string word;
+	int i;
 
-    cout << "Enter a string: ";
-    getline(word, 100);
+    // ask user input for a set of strings
+	cout << "Enter a string: ";
+	getline(cin, word);
 
+	string lastWord;
+    // for loop to go to each string and print the last word
+	for (i = word.length() - 1; i >= 0; i--) {
+        // if else statement that if the string is a space, print the last word
+		if (word[i] == ' ') {
+			cout << lastWord << endl;
+			lastWord = "";
+		} else {
+			lastWord = word[i] + lastWord;
+		}
+	}
 
+	cout << lastWord;
+    return 0;
 }
