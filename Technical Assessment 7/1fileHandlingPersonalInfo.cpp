3fcded427f28a5a68cc233d22cf6ff9f8192bf51
rei <202210643@fit.edu.ph>
2023-03-19 11:08:24 +0800

    created the file for tsa7 file handling

---
+++ Technical Assessment 7/1fileHandlingPersonalInfo.cpp
@@ -0,0 +1,22 @@
+/*
+Create a C++ program which shows the given menu items:
+    1. Create  (The create option will create a text file named "mytext.txt").
+
+         * This option should save accept basic personal information of the user such as name, course, email and contact number.
+
+    2. Read (The read option will read the contents of the file.)
+
+    3. Append (The append option will add text contents in mytext.txt)
+
+        * This option should add a string inputted by the user
+
+    4. Exit (The exit option will terminate the program which means the program is in loop while running)
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    
+}
